395f9bca5667eeef58ff3d4d4c738cfb2801113e
wuchao <wuchao@apusapps.com>
2024-09-22 22:12:54 +0800

    使用Catalogs + Groovy DSL的方式管理版本，并且封装，去除gradle.kts

--- build.gradle.kts
+++
@@ -1,6 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    // 使用 buildSrc 中的插件，不注释会冲突
-//    id("com.android.application") version "8.1.4" apply false
-//    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-}
